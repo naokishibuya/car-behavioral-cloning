0cb1f7de64a56209dbf9bad21a79b7e6a9fbc83c
Naoki Shibuya <naoki.quant@gmail.com>
2017-02-14 22:00:57 +0800

    [update] parameters can be specified at command line

--- model.py
+++ model.py
@@ -5,31 +5,31 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
-from utils import DRIVING_LOG_PATH, INPUT_SHAPE, batch_generator
+from utils import INPUT_SHAPE, batch_generator
+import argparse
+import os
 
 np.random.seed(0)
 
 
-def load_data(path):
+def load_data(args):
     """
     Load training data and split it into training and validation set
     """
-    data_df = pd.read_csv(path)
+    data_df = pd.read_csv(os.path.join(args.data_dir, 'driving_log.csv'))
 
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
 
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
 
     return X_train, X_valid, y_train, y_valid
 
 
-def build_model():
+def build_model(args):
     """
     Modified NVIDIA model
     """
-    keep_prob = 0.5
-
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
     model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
@@ -37,7 +37,7 @@ def build_model():
     model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
     model.add(Conv2D(64, 3, 3, activation='elu'))
     model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Dropout(keep_prob))
+    model.add(Dropout(args.keep_prob))
     model.add(Flatten())
     model.add(Dense(100, activation='elu'))
     model.add(Dense(50, activation='elu'))
@@ -48,39 +48,63 @@ def build_model():
     return model
 
 
-def train_model(model, X_train, X_valid, y_train, y_valid):
+def train_model(model, args, X_train, X_valid, y_train, y_valid):
     """
     Train the model
     """
-    batch_size = 40
-    samples_per_epoch = 20000
-    nb_epoch = 10
-
-    checkpoint = ModelCheckpoint("model-{epoch:03d}.h5",
+    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
                                  monitor='val_loss',
                                  verbose=0,
-                                 save_best_only=True,
+                                 save_best_only=args.save_best_only,
                                  mode='auto')
 
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=1.0e-4))
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
 
-    model.fit_generator(batch_generator(X_train, y_train, batch_size, True),
-                        samples_per_epoch,
-                        nb_epoch,
+    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),
+                        args.samples_per_epoch,
+                        args.nb_epoch,
                         max_q_size=1,
-                        validation_data=batch_generator(X_valid, y_valid, batch_size, False),
+                        validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),
                         nb_val_samples=len(X_valid),
                         callbacks=[checkpoint],
                         verbose=1)
+
+
+def s2b(s):
+    """
+    Converts a string to boolean value
+    """
+    s = s.lower()
+    return s == 'true' or s == 'yes' or s == 'y' or s == '1'
+
 
 def main():
     """
     Load train/validation data set and train the model
     """
-    data = load_data(DRIVING_LOG_PATH)
-    model = build_model()
-    train_model(model, *data)
+    parser = argparse.ArgumentParser(description='Behavioral Cloning Training Program')
+    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
+    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
+    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
+    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
+    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
+    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
+    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
+    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
+    args = parser.parse_args()
+
+    print('-' * 30)
+    print('Parameters')
+    print('-' * 30)
+    for key, value in vars(args).items():
+        print('{:<20} := {}'.format(key, value))
+    print('-' * 30)
+
+    data = load_data(args)
+    model = build_model(args)
+    train_model(model, args, *data)
 
 
 if __name__ == '__main__':
     main()
+
--- utils.py
+++ utils.py
@@ -2,20 +2,16 @@ import cv2, os
 import numpy as np
 import matplotlib.image as mpimg
 
-IMAGE_DIR = 'data'
-DRIVING_LOG_PATH = os.path.join(IMAGE_DIR, 'driving_log.csv')
 
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 
 
-def load_image(file):
+def load_image(data_dir, image_file):
     """
     Load RGB images from a file
     """
-    path = os.path.join(IMAGE_DIR, file.strip())
-    image = mpimg.imread(path)
-    return image
+    return mpimg.imread(os.path.join(data_dir, image_file.strip()))
 
 
 def crop(image):
@@ -49,17 +45,17 @@ def preprocess(image):
     return image
 
 
-def choose_image(center, left, right, steering_angle):
+def choose_image(data_dir, center, left, right, steering_angle):
     """
     Randomly choose an image from the center, left or right, and adjust
     the steering angle.
     """
     choice = np.random.choice(3)
     if choice == 0:
-        return load_image(left), steering_angle + 0.2
+        return load_image(data_dir, left), steering_angle + 0.2
     elif choice == 1:
-        return load_image(right), steering_angle - 0.2
-    return load_image(center), steering_angle
+        return load_image(data_dir, right), steering_angle - 0.2
+    return load_image(data_dir, center), steering_angle
 
 
 def random_flip(image, steering_angle):
@@ -96,19 +92,19 @@ def random_brightness(image):
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
 
-def augument(center, left, right, steering_angle, range_x=100, range_y=10):
+def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y=10):
     """
     Generate an augumented image and adjust steering angle.
     (The steering angle is associated with the center image)
     """
-    image, steering_angle = choose_image(center, left, right, steering_angle)
+    image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
     image, steering_angle = random_flip(image, steering_angle)
     image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
     image = random_brightness(image)
     return image, steering_angle
 
 
-def batch_generator(image_paths, steering_angles, batch_size, is_training):
+def batch_generator(data_dir, image_paths, steering_angles, batch_size, is_training):
     """
     Generate training image give image paths and associated steering angles
     """
@@ -120,10 +116,10 @@ def batch_generator(image_paths, steering_angles, batch_size, is_training):
             center, left, right = image_paths[index]
             steering_angle = steering_angles[index]
             # argumentation
-            if is_training and np.random.rand() < 0.7:
-                image, steering_angle = augument(center, left, right, steering_angle)
+            if is_training and np.random.rand() < 0.6:
+                image, steering_angle = augument(data_dir, center, left, right, steering_angle)
             else:
-                image = load_image(center) 
+                image = load_image(data_dir, center) 
             # add the image and steering angle to the batch
             images[i] = preprocess(image)
             steers[i] = steering_angle
