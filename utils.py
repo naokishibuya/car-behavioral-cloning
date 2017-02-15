3604a57ec215f749e9e32a5656391c5da0f9f48f
Naoki Shibuya <naoki.quant@gmail.com>
2017-02-15 13:36:44 +0800

    added random shadows

--- utils.py
+++ utils.py
@@ -81,6 +81,34 @@ def random_translate(image, steering_angle, range_x, range_y):
     return image, steering_angle
 
 
+def random_shadow(image):
+    """
+    Generates and adds random shadow
+    """
+    # (x1, y1) and (x2, y2) forms a line
+    # xm, ym gives all the locations of the image
+    x1, y1 = IMAGE_WIDTH * np.random.rand(), 0
+    x2, y2 = IMAGE_WIDTH * np.random.rand(), IMAGE_HEIGHT
+    xm, ym = np.mgrid[0:IMAGE_HEIGHT, 0:IMAGE_WIDTH]
+
+    # mathematically speaking, we want to set 1 below the line and zero otherwise
+    # Our coordinate is up side down.  So, the above the line: 
+    # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
+    # as x2 == x1 causes zero-division problem, we'll write it in the below form:
+    # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
+    mask = np.zeros_like(image[:, :, 1])
+    mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
+
+    # choose which side should have shadow and adjust saturation
+    cond = mask == np.random.randint(2)
+    s_ratio = np.random.uniform(low=0.2, high=0.5)
+
+    # adjust Saturation in HLS(Hue, Light, Saturation)
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
+    return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
+
+
 def random_brightness(image):
     """
     Randomly adjust brightness of the image.
@@ -100,6 +128,7 @@ def augument(data_dir, center, left, right, steering_angle, range_x=100, range_y
     image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
     image, steering_angle = random_flip(image, steering_angle)
     image, steering_angle = random_translate(image, steering_angle, range_x, range_y)
+    image = random_shadow(image)
     image = random_brightness(image)
     return image, steering_angle
 
